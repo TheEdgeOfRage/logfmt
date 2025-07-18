ea2e178c0a1efaca8a6596a20c4aa7d1545de553
Jakub <jakub.izdebski@jrdltd.co.uk>
2025-07-18 09:25:50 +0200

    add option to skip empty and enforce fields order

--- config/config.go
+++ config/config.go
@@ -21,6 +21,8 @@ type Config struct {
 	LogLevel int
 	// OutputFields is a list of fields to include on the log output
 	OutputFields []string
+	// OrderOutputFields is a list of fields to include on the log output in order
+	OrderOutputFields []string
 	// ExcludeFields is a list of fields to exclude from the log output
 	ExcludeFields []string
 	// Filter is a map of fields and values which are used to filter the log output
@@ -32,14 +34,14 @@ type Config struct {
 }
 
 type rawConfig struct {
-	LogLevel      		string `long:"level" short:"l" description:"Log level filter. One of DEBUG, INFO, WARN, ERROR, FATAL" default:"INFO"` // nolint:lll
-	OutputFields  		string `long:"output" short:"o" description:"Output field selector (comma separated)"`
-	ExcludeFields 		string `long:"exclude" short:"e" description:"Exclude field selector (comma separated)"`
-	Filter        		string `long:"filter" short:"f" description:"Filter fields (key=value comma separated)"`
-	NoColor       		bool   `long:"no-color" short:"n" description:"Disable color output"`
-	ForceColor    		bool   `long:"force-color" short:"c" description:"Force color output, even when outputting to a pipe"`
-	NoTime        		bool   `long:"no-time" short:"t" description:"Disable time output"`
-	KeepEmpty		bool   `long:"keep-empty" short:"k" description:"Keep lines with no field present selected by output or with all excluded"`
+	LogLevel      string `long:"level" short:"l" description:"Log level filter. One of DEBUG, INFO, WARN, ERROR, FATAL" default:"INFO"` // nolint:lll
+	OutputFields  string `long:"output" short:"o" description:"Output field selector (comma separated)"`
+	ExcludeFields string `long:"exclude" short:"e" description:"Exclude field selector (comma separated)"`
+	Filter        string `long:"filter" short:"f" description:"Filter fields (key=value comma separated)"`
+	NoColor       bool   `long:"no-color" short:"n" description:"Disable color output"`
+	ForceColor    bool   `long:"force-color" short:"c" description:"Force color output, even when outputting to a pipe"`
+	NoTime        bool   `long:"no-time" short:"t" description:"Disable time output"`
+	KeepEmpty     bool   `long:"keep-empty" short:"k" description:"Keep lines with no field present selected by output or with all excluded"`
 }
 
 func Parse() (*Config, error) {
@@ -51,7 +53,7 @@ func Parse() (*Config, error) {
 		return nil, err
 	}
 
-	if raw.ExcludeFields != "" && raw.OutputFields != ""  {
+	if raw.ExcludeFields != "" && raw.OutputFields != "" {
 		return nil, fmt.Errorf("cannot use both --exclude and --output")
 	}
 
